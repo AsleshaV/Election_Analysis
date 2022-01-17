8d05623d9cd86403619fb72ae6efc32a3d661055
unknown <aspirantash@icloud.com>
2022-01-16 18:26:16 -0600

    Adding PyPoll.py file.

---
+++ hello.phy.py
@@ -0,0 +1,31 @@
+# Create a variable called 'name' that holds a string
+
+name="Riaan"
+# Create a variable called 'country' that holds a string
+
+country="USA"
+# Create a variable called 'age' that holds an integer
+age= 25
+
+# Create a variable called 'hourly_wage' that holds an integer
+hourly_wage = 14
+
+# Calculate the daily wage for the user
+daily_wage = 14*8
+# Create a variable called 'satisfied' that holds a boolean
+satisfied = True
+
+# Print out "Hello <name>!"
+print ("Hello" + Riaan)
+
+# Print out what country the user entered
+print ("I live in"+ USA)
+
+# Print out the user's age
+print ("my age is"+ 25)
+
+# With an f-string, print out the daily wage that was calculated
+
+print(f" i make {112} per day")
+# With an f-string, print out whether the users were satisfied
+print("Are you satisifed? {satisfied}")
